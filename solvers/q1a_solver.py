5d71f2c36931f1d39d7423094aabfa701b67c0c2
Teng Kong Cheng <kten0008@student.monash.edu>
2024-08-26 13:19:35 +0800

    Remove print statement form do not modify hedaer

--- solvers/q1a_solver.py
+++ solvers/q1a_solver.py
@@ -15,7 +15,6 @@ def q1a_solver(problem: q1a_problem):
         num_expansions += 1
         terminate, result = astar_loop_body(problem, astarData)
     print(f'Number of node expansions: {num_expansions}')
-    print(result)
     return result
 
 #-------------------#
