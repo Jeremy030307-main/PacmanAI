b4f23ff2bab2453ffaade1ddfb75492f3fcdeb0d
Teng Kong Cheng <kten0008@student.monash.edu>
2024-08-31 23:49:23 +0800

    Initial implementation on q2

--- agents/q2Agent.py
+++ agents/q2Agent.py
@@ -45,5 +45,69 @@ class Q2_Agent(Agent):
         """
         logger = logging.getLogger('root')
         logger.info('MinimaxAgent')
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        
+        actions = gameState.getLegalActions(0)
+        currentScore = float('-inf')
+        returnAction = None
+        alpha = float('-inf')
+        beta = float('inf')
+
+        for action in actions:
+            nextState = gameState.generateSuccessor(0,action)
+            # Next level is a min level. Hence calling min for successors of the root.
+            score = self.min_value(nextState,0,alpha,beta, 1)
+
+            # Choosing the action which is Maximum of the successors.
+            if score > currentScore:
+                returnAction = action
+                currentScore = score
+            # Updating alpha value at root.    
+            if score > beta:
+                return returnAction
+            alpha = max(alpha,score)
+
+        return returnAction
+
+    def max_value(self, game_state: GameState, depth: int, alpha: int, beta:int):
+
+        currDepth = depth + 1
+
+        # Check for termina state
+        if game_state.isWin() or game_state.isLose() or currDepth == self.depth:  
+            return self.evaluationFunction(game_state)
+    
+        max_value = float('-inf')
+        actions = game_state.getLegalActions(0)
+        for action in actions:
+
+            # generate the successor game state after taking this action
+            successor= game_state.generateSuccessor(0,action)
+
+            max_value = max (max_value, self.min_value(successor, currDepth, alpha, beta))
+            if max_value > beta:
+                return max_value
+            alpha = max(alpha,max_value)
+        return max_value
+
+    def min_value(self, game_state: GameState, depth: int, alpha: int, beta:int, agent_index = 1):
+
+        # Check for terminal state
+        if game_state.isWin() or game_state.isLose(): 
+            return self.evaluationFunction(game_state)
+        
+        minvalue = float('inf')
+        actions = game_state.getLegalActions(1)
+        for action in actions:
+            successor= game_state.generateSuccessor(1,action)
+
+            if agent_index == (game_state.getNumAgents()-1):
+                minvalue = min(minvalue,self.max_value(successor,depth,alpha,beta))
+            else:
+                minvalue = min(minvalue,self.min_value(successor,depth,alpha,beta, agent_index+1))
+            
+            if minvalue <= alpha:
+                return minvalue
+            
+            beta = min(beta,minvalue)
+
+        return minvalue
